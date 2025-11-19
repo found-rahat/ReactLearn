e737b66db1375261ef776ea970d1acc5d7c5fbc3
Md Rahat Ahmed <foundrahat1999@gmail.com>
2025-11-19 21:05:48 +0600

    functional components

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,6 @@
 import React from "react";
-function Greet(name) {
-  return <h1>Hello Rahat Ahmed</h1>;
-}
-export default Greet;
+// function Greet(name) {
+//   return <h1>Hello Rahat Ahmed</h1>;
+// }
+export const Greet = () => <h1>Hello Ahmed</h1>;
+// export default Greet;

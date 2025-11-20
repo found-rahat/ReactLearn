5dabffc27ad4eff43de54af25746ac0ab32d7363
Md Rahat Ahmed <foundrahat1999@gmail.com>
2025-11-20 21:41:12 +0600

    use state with subscribe button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/welcome";
 import Withjsx from "./components/jsx";
+import State from "./components/state";
 
 function App() {
   return (
@@ -10,10 +11,11 @@ function App() {
       {/* <Greet name="Captain Bangladesh" heroName="Rahat" Age="25" />
       <Greet name="SpiderWoman" heroName="Khan" Age="30" />
       <Greet name="WonderMan" heroName="Joy" Age="10" /> */}
-      <Welcome name="Captain Bangladesh" heroName="Rahat" Age="25" />
+      {/* <Welcome name="Captain Bangladesh" heroName="Rahat" Age="25" />
       <Welcome name="SpiderWoman" heroName="Khan" Age="30" />
-      <Welcome name="WonderMan" heroName="Joy" Age="10" />
+      <Welcome name="WonderMan" heroName="Joy" Age="10" /> */}
       {/* <Withjsx /> */}
+      <State />
     </div>
   );
 }
---
+++ src/components/state.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export class state extends Component {
+  constructor() {
+    super();
+    this.state = {
+      massage: "Welcome Visitor",
+      button: "Subscribe",
+    };
+  }
+  changeMasage() {
+    if (this.state.button === "Subscribe") {
+      this.setState({
+        massage: "thank You for Subscribing ....",
+        button: "unsubscribe",
+      });
+    } else {
+      this.setState({
+        massage: "Welcome Visitor",
+        button: "Subscribe",
+      });
+    }
+  }
+  render() {
+    return (
+      <>
+        <h1>{this.state.massage}</h1>
+        <button onClick={() => this.changeMasage()}>{this.state.button}</button>
+      </>
+    );
+  }
+}
+
+export default state;
